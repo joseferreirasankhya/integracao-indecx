0f43f40e122e69226c8bd6aa1920d5d52d9cd0fc
joseferreirasankhya <jose.ferreira@sankhya.com.br>
2025-01-29 14:46:22 -0300

    Adding debug/ route

--- sensedata/views.py
+++ sensedata/views.py
@@ -1,12 +1,21 @@
+# Imports
+# - Django
 from django.shortcuts import render
+
+# - Rest Framework
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+# Views
+
+# - Index
 @api_view(['GET'])
 def index(request):
     return Response({'message': 'Hello, World!'}, status=status.HTTP_200_OK)
 
-@api_view(['GET'])
-def ping(request):
-    return Response({'message': 'pong'}, status=status.HTTP_200_OK)
+# - Debug
+@api_view(['POST'])
+def debug(request):
+    print(request.data)
+    return Response({'message': 'Debug', 'data': request.data}, status=status.HTTP_200_OK)
