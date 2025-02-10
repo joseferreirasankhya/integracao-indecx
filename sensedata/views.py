f427b6fc8ca4fbd9af112133f1c88b4bda156d03
joseferreirasankhya <jose.ferreira@sankhya.com.br>
2025-02-10 17:41:45 -0300

    Adding logging

--- sensedata/views.py
+++ sensedata/views.py
@@ -5,13 +5,17 @@ from rest_framework.decorators import api_view
 from sensedata.services.nps_service import NPSService
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
+
+logger = logging.getLogger(__name__)
 
 nps_service = NPSService(
     api_url=os.getenv('SENSE_NPS_API_URL'),
     api_key=f"{os.getenv('SENSE_NPS_API_KEY')}="
 )
+
 
 @api_view(['GET'])
 def index(request):
@@ -21,11 +25,17 @@ def index(request):
 @api_view(['POST'])
 def debug_nps(request):
     """Debug endpoint for NPS data transformation"""
+    
+    # Logando o request data
+    logger.info("Request data received: %s", request.data)
+    
     if not request.data:
+        logger.warning("No data provided in the request.")
         return Response(
             {'message': 'No data provided'}, 
             status=status.HTTP_400_BAD_REQUEST
         )
+    
     return Response(request.data, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
