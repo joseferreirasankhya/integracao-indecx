7ecc715acaad2e1a9d4718db03755434c109ac47
joseferreirasankhya <jose.ferreira@sankhya.com.br>
2025-03-19 10:43:24 -0300

    Consertando envio de dados para o MongoDB

--- zendesk/mongo/mongo_client.py
+++ zendesk/mongo/mongo_client.py
@@ -7,29 +7,29 @@ load_dotenv()
 class MongoClientInterface:
     def __init__(self):
         self.uri = f"mongodb+srv://{os.environ.get('MONGODB_USER')}:{os.environ.get('MONGODB_PASSWORD')}@{os.environ.get('MONGODB_HOST')}/?retryWrites=true&w=majority"
-        # self.uri = "mongodb+srv://joseferreira:f2UMT3hzlqro4np8@csat.in72g.mongodb.net/?retryWrites=true&w=majority"
-        self.client = MongoClient(self.uri)
+        # self.client = MongoClient(self.uri)
+        self.database = os.environ.get("MONGODB_DATABASE")
+        self.collection = os.environ.get("MONGODB_COLLECTION")
+
+    def get_client(self):
+        """Garante que um novo cliente é criado sempre que necessário"""
+        return MongoClient(self.uri)
 
     def test(self):
+        """Testa se a conexão com o banco funciona"""
         try:
-            print(self.client.server_info())  # Isso forçará a conexão e mostrará info do servidor
+            client = self.get_client()
+            print(client.server_info())  # Isso forçará a conexão e mostrará info do servidor
+            client.close()
         except Exception as e:
             print(f"Erro ao conectar: {e}")
 
-    def connect(self):
-        # Send a ping to confirm a successful connection
-        try:
-            self.client.admin.command('ping')
-            print("Pinged your deployment. You successfully connected to MongoDB!")
-            return self.client
-        except Exception as e:
-            print(e)
-
     def save_document(self, document):
-        conn = self.connect()
-        database = conn[os.environ.get("MONGODB_DATABASE")]
-        collection = database[os.environ.get("MONGODB_COLLECTION")]
-        response = collection.insert_one(document)
-        conn.close()
+        """Salva o documento no banco"""
+        with self.get_client() as conn:
+            database = conn[self.database]
+            collection = database[self.collection]
+            response = collection.insert_one(document)
+            conn.close()
 
-        return response
+            return response
