afd6b9e0636e7368a112935df885316cbd40b4b7
joseferreirasankhya <jose.ferreira@sankhya.com.br>
2025-03-21 14:23:24 -0300

    Removendo módulo Zendesk temporariamente (não funcionando corretamente)

--- zendesk/urls.py
+++
@@ -1,8 +0,0 @@
-from zendesk import views
-from django.urls import path
-
-# URL patterns
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('save-data/', views.process_ticket_data, name='save-data')
-]
